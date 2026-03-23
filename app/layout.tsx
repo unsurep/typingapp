d1846c99c1290a9c09e833bc71a143b7c5a01c4c
Oghenevwede Louis Umukoro <168831499+unsurep@users.noreply.github.com>
2026-03-23 12:45:50 +0100

    added google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,6 +75,18 @@ new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-M5PW9M8G');`}</Script>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-1TYMJZK2S3"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-1TYMJZK2S3');
+        `}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col overflow-x-hidden`}
       >
