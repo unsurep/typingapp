d8b8b699400bd1ae5440946a470063c98fdf047f
yireh7519-design <yireh7519@gmail.com>
2026-05-11 21:30:45 -0500

    Update page.tsx

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -14,6 +14,7 @@ export async function generateMetadata({
     return {
         title: t("metaTitle"),
         description: t("metaDescription"),
+                alternates: { canonical: 'https://www.typingverified.com/about' },
     };
 }
 
